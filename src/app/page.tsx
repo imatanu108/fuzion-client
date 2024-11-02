2177739aeef5f35000536803b2f2549dfacc70ed
Atanu Hazra <imatanuhazra@gmail.com>
2024-11-02 12:13:51 +0530

    null

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
   return (
     <div className="flex justify-center items-center">
-      <div className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600 animate-pulse drop-shadow-[0_0_20px_rgba(128,0,128,0.8)]">
+      <div className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 dark:from-blue-700 to-purple-600 dark:to-purple-600 animate-pulse drop-shadow-[0_0_20px_rgba(128,0,128,0.8)]">
         Under Development...
       </div>
     </div>
