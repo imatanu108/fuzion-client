04ddc184060e9006997d97ca67282bae93ce72d2
Atanu Hazra <imatanuhazra@gmail.com>
2024-11-25 12:55:28 +0530

    fix form-ui

--- src/components/user/VerifyEmailForm.tsx
+++ src/components/user/VerifyEmailForm.tsx
@@ -107,7 +107,7 @@ const VerifyEmailForm: React.FC = () => {
     }, []);
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="flex flex-col items-center min-h-screen">
             <Form {...form}>
                 <form onSubmit={handleSubmit} className="space-y-6 p-6 rounded md:shadow-md w-[100%] md:w-[50%] lg:w-[45%]">
                     <h2 className="text-2xl font-semibold text-blue-500 text-center mb-6">Verify Your Email</h2>
